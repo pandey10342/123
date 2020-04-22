15979bba0dbe7b687ea0df2437e282f2ee76fc6f
unknown <rohitp@techepoch.com>
2020-04-22 15:57:28 +0530

    first commit

--- platforms/android/app/src/main/assets/www/cordova_plugins.js
+++ platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -16,54 +16,6 @@ module.exports = [
       "window.admob",
       "window.tappx"
     ]
-  },
-  {
-    "id": "cordova-plugin-actionsheet.ActionSheet",
-    "file": "plugins/cordova-plugin-actionsheet/www/ActionSheet.js",
-    "pluginId": "cordova-plugin-actionsheet",
-    "clobbers": [
-      "window.plugins.actionsheet"
-    ]
-  },
-  {
-    "id": "cordova-plugin-dialogs.notification",
-    "file": "plugins/cordova-plugin-dialogs/www/notification.js",
-    "pluginId": "cordova-plugin-dialogs",
-    "merges": [
-      "navigator.notification"
-    ]
-  },
-  {
-    "id": "cordova-plugin-dialogs.notification_android",
-    "file": "plugins/cordova-plugin-dialogs/www/android/notification.js",
-    "pluginId": "cordova-plugin-dialogs",
-    "merges": [
-      "navigator.notification"
-    ]
-  },
-  {
-    "id": "uk.co.workingedge.phonegap.plugin.launchnavigator.Common",
-    "file": "plugins/uk.co.workingedge.phonegap.plugin.launchnavigator/www/common.js",
-    "pluginId": "uk.co.workingedge.phonegap.plugin.launchnavigator",
-    "clobbers": [
-      "launchnavigator"
-    ]
-  },
-  {
-    "id": "uk.co.workingedge.phonegap.plugin.launchnavigator.LocalForage",
-    "file": "plugins/uk.co.workingedge.phonegap.plugin.launchnavigator/www/localforage.v1.5.0.min.js",
-    "pluginId": "uk.co.workingedge.phonegap.plugin.launchnavigator",
-    "clobbers": [
-      "localforage"
-    ]
-  },
-  {
-    "id": "uk.co.workingedge.phonegap.plugin.launchnavigator.LaunchNavigator",
-    "file": "plugins/uk.co.workingedge.phonegap.plugin.launchnavigator/www/android/launchnavigator.js",
-    "pluginId": "uk.co.workingedge.phonegap.plugin.launchnavigator",
-    "merges": [
-      "launchnavigator"
-    ]
   }
 ];
 module.exports.metadata = 
@@ -71,10 +23,7 @@ module.exports.metadata =
 {
   "cordova-connectivity-monitor": "1.2.2",
   "cordova-play-services-version-adapter": "1.0.2",
-  "cordova-admob": "5.1.0",
-  "cordova-plugin-actionsheet": "2.3.3",
-  "cordova-plugin-dialogs": "2.0.2",
-  "uk.co.workingedge.phonegap.plugin.launchnavigator": "5.0.4"
+  "cordova-admob": "5.1.0"
 };
 // BOTTOM OF METADATA
 });
